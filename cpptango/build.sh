a383257813097508cac6f3fbeb57ecc72bbf2eaf
Reynald Bourtembourg <bourtemb@users.noreply.github.com>
2020-03-30 09:22:16 +0200

    Add cpptango and tango-idl conda packages - First GitHub workflow (#1)
    * Add tango-idl conda recipe
    * Add cpptango conda recipe for cppTango 9.3.4rc2
    * Add Github workflow to build cpptango and tango-idl conda packages

---
+++ cpptango/build.sh
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_VERBOSE_MAKEFILE=true \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DOMNI_BASE=$PREFIX \
+      -DZMQ_BASE=$PREFIX \
+      -DIDL_BASE=$PREFIX \
+      -DBUILD_TESTING=OFF \
+      ..
+
+make -j `nproc` install
